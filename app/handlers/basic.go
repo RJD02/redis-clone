3ad15339f50635bb1196431d9d2bb37004df884b
RJD02 <dulangeraviraj@gmail.com>
2025-10-22 17:56:00 +0000

    codecrafters submit [skip ci]

--- app/handlers/basic.go
+++ app/handlers/basic.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/hex"
 	"fmt"
 	"net"
 
@@ -64,4 +65,33 @@ func HandlePsync(conn net.Conn, cmd *Command) {
 		return
 	}
 	fmt.Printf("PSYNC: %v -> FULLRESYNC %s %d\n", cmd.Args, replId, replOffset)
+
+	// Send empty RDB file after FULLRESYNC response
+	// This is the hex representation of an empty RDB file
+	emptyRDBHex := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+	
+	// Decode hex to binary
+	rdbData, err := hex.DecodeString(emptyRDBHex)
+	if err != nil {
+		fmt.Printf("Failed to decode RDB hex: %v\n", err)
+		return
+	}
+
+	// Send RDB file in the format: $<length>\r\n<binary_contents>
+	// Note: This is NOT a RESP bulk string, so no trailing \r\n
+	rdbResponse := fmt.Sprintf("$%d\r\n", len(rdbData))
+	_, err = conn.Write([]byte(rdbResponse))
+	if err != nil {
+		fmt.Printf("Failed to write RDB length: %v\n", err)
+		return
+	}
+
+	// Send the binary RDB data
+	_, err = conn.Write(rdbData)
+	if err != nil {
+		fmt.Printf("Failed to write RDB data: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Sent empty RDB file (%d bytes)\n", len(rdbData))
 }
